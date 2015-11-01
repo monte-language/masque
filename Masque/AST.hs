3cc3bb28aaea5a6a003bb8f965b9a4ac5e862e05
Corbin Simpson <cds@corbinsimpson.com>
2015-10-31 17:50:14 -0700

    Rework AST loading to load new-mast format.

--- Masque/AST.hs
+++ Masque/AST.hs
@@ -1,74 +1,59 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE RecursiveDo #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE ViewPatterns #-}
-
 module Masque.AST where
 
 import Control.Applicative
-import Control.Lens
 import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.State
 import Data.Binary.Get
 import Data.Binary.IEEE754
 import Data.Bits
 import qualified Data.ByteString.Char8 as BSC
-import Data.Data
-import Data.Data.Lens
-import Data.List
-import Data.Word
-import Text.PrettyPrint.GenericPretty
+import Data.Foldable
+import Data.List (genericIndex)
 
-data Node = Null
-          | CharNode Char
-          | DoubleNode Double
-          | IntNode Integer
-          | StrNode String
-          | Tuple [Node]
-          | Assign String Node
-          | BindingNode String
-          | Call Node Node Node
-          | Def Pattern Node Node
-          | Escape Pattern Node Pattern Node
-          | Finally Node Node
-          | Hide Node
-          | If Node Node Node
-          | Matcher Pattern Node
-          | Method Node Node [Pattern] Node Node
-          | Noun String
-          | Object Node Pattern Node Node
-          | Script Node [Node] [Node]
-          | Sequence [Node]
-          | Try Node Pattern Node
-    deriving (Data, Eq, Generic, Show, Typeable)
+data Expr = NullExpr
+          | CharExpr Char
+          | DoubleExpr Double
+          | IntExpr Integer
+          | StrExpr String
+          | AssignExpr String Expr
+          | BindingExpr String
+          | CallExpr Expr String [Expr] [NamedExpr]
+          | DefExpr Patt Expr Expr
+          | EscapeOnlyExpr Patt Expr
+          | EscapeExpr Patt Expr Patt Expr
+          | FinallyExpr Expr Expr
+          | HideExpr Expr
+          | IfExpr Expr Expr Expr
+          | MatcherExpr Patt Expr
+          | MethodExpr String String [Patt] [NamedPatt] Expr Expr
+          | NounExpr String
+          | ObjectExpr String Patt Expr [Expr] [Expr] [Expr]
+          | SequenceExpr [Expr]
+          | TryExpr Expr Patt Expr
+    deriving (Eq, Show, Read)
 
-instance Out Node
-instance Plated Node where
-    plate = uniplate
+data NamedExpr = NamedExpr Expr Expr
+    deriving (Eq, Show, Read)
 
-data Pattern = Ignore Node
-             | BindPattern String
-             | Final String Node
-             | ListPattern [Pattern]
-             | Var String Node
-             | Via Node Pattern
-    deriving (Data, Eq, Generic, Show, Typeable)
+data Patt = IgnorePatt Expr
+          | BindPatt String
+          | FinalPatt String Expr
+          | ListPatt [Patt]
+          | VarPatt String Expr
+          | ViaPatt Expr Patt
+    deriving (Eq, Show, Read)
 
-instance Out Pattern
-instance Plated Pattern where
-    plate = uniplate
+data NamedPatt = NamedPatt Expr Patt Expr
+    deriving (Eq, Show, Read)
 
 -- | Deserialization
 
-unshift :: Word8 -> Word8
-unshift = subtract 32
+type MASTContext = ([Expr], [Patt])
 
-getByte :: Get Word8
-getByte = liftM unshift getWord8
-
-getVarInt :: Get Integer
+getVarInt :: StateT MASTContext Get Integer
 getVarInt = do
-    b <- getByte
+    b <- lift getWord8
     let rv = toInteger $ b .&. 0x7f
     if b .&. 0x80 == 0x00
         then return rv
@@ -76,93 +61,102 @@ getVarInt = do
             vi <- getVarInt
             return $ rv .|. vi `shiftL` 7
 
-getDouble :: Get Double
+getDouble :: StateT MASTContext Get Double
 getDouble = do
-    bs <- replicateM 8 getByte
+    bs <- replicateM 8 (lift getWord8)
     let w64 = foldl' (\w x -> w `shiftL` 8 .|. fromIntegral x) 0 bs
     return $ wordToDouble w64
 
 zzd :: Integer -> Integer
 zzd i = if i .&. 1 == 1 then (i `div` 2) `xor` (-1) else i `div` 2
 
-getPatternList :: Get [Pattern]
-getPatternList = do
-    7 <- getByte
-    arity <- getVarInt
-    replicateM (fromIntegral arity) getPattern
+-- XXX UTF-8 decode
+getStr :: StateT MASTContext Get String
+getStr = do
+    len <- getVarInt
+    bs <- lift $ getByteString (fromIntegral len)
+    return $ BSC.unpack bs
 
-getPattern :: Get Pattern
-getPattern = do
-    tag <- getByte
-    case tag of
-        -- null pattern
-        0  -> return $ Ignore Null
-        27 -> Final <$> getNoun <*> getNode
-        28 -> Ignore <$> getNode
-        29 -> Var <$> getNoun <*> getNode
-        30 -> do
-            -- Must be a tuple
-            ps <- getPatternList
-            -- Discard the tail
-            _ <- getPattern
-            return $ ListPattern ps
-        31 -> Via <$> getNode <*> getPattern
-        32 -> BindPattern <$> getNoun
-        _  -> fail "Not a pattern"
+getExpr :: StateT MASTContext Get Expr
+getExpr = do
+    i <- getVarInt
+    gets (\(exprs, _) -> exprs `genericIndex` i)
 
-getNoun :: Get String
-getNoun = do
-    node <- getNode
-    case node of
-        StrNode s -> return s
-        Noun s    -> return s
-        _         -> fail "Not a string or noun"
+getExprs :: StateT MASTContext Get [Expr]
+getExprs = do
+    i <- getVarInt
+    replicateM (fromInteger i) getExpr
 
-getNode :: Get Node
-getNode = do
-    tag <- getByte
-    case tag of
-        0  -> return Null
-        3  -> do
-            len <- getVarInt
-            bs <- getByteString $ fromIntegral len
-            return $ StrNode $ BSC.unpack bs
-        4  -> DoubleNode <$> getDouble
-        -- 5  -> CharNode
-        6  -> do
-            i <- getVarInt
-            return $ IntNode $ zzd i
-        7  -> do
-            arity <- getVarInt
-            ps <- replicateM (fromIntegral arity) getNode
-            return $ Tuple ps
-        -- Literals contain a single literal node
-        10 -> getNode
-        11 -> Noun <$> getNoun
-        12 -> BindingNode <$> getNoun
-        13 -> do
-            Tuple ns <- getNode
-            return $ Sequence ns
-        14 -> Call <$> getNode <*> getNode <*> getNode
-        15 -> Def <$> getPattern <*> getNode <*> getNode
-        16 -> Escape <$> getPattern <*> getNode <*> getPattern <*> getNode
-        17 -> Object <$> getNode <*> getPattern <*> getNode <*> getNode
-        18 -> do
-            s <- Script <$> getNode
-            Tuple methods <- getNode
-            Tuple matchers <- getNode
-            return $ s methods matchers
-        19 -> do
-            m <- Method <$> getNode <*> getNode
-            ps <- getPatternList
-            m ps <$> getNode <*> getNode
-        20 -> Matcher <$> getPattern <*> getNode
-        21 -> Assign <$> getNoun <*> getNode
-        22 -> Finally <$> getNode <*> getNode
-        23 -> Try <$> getNode <*> getPattern <*> getNode
-        24 -> Hide <$> getNode
-        25 -> If <$> getNode <*> getNode <*> getNode
-        33 -> do
-            StrNode [c] <- getNode
-            return $ CharNode c
-        x  -> fail $ "Not a node: " ++ show x
+getNamedExprs :: StateT MASTContext Get [NamedExpr]
+getNamedExprs = do
+    i <- getVarInt
+    replicateM (fromInteger i) $ do
+        k <- getExpr
+        v <- getExpr
+        return $ NamedExpr k v
+
+getPatt :: StateT MASTContext Get Patt
+getPatt = do
+    i <- getVarInt
+    gets (\(_, patts) -> patts `genericIndex` i)
+
+getPatts :: StateT MASTContext Get [Patt]
+getPatts = do
+    i <- getVarInt
+    replicateM (fromInteger i) getPatt
+
+getNamedPatts :: StateT MASTContext Get [NamedPatt]
+getNamedPatts = do
+    i <- getVarInt
+    replicateM (fromInteger i) $ do
+        k <- getExpr
+        p <- getPatt
+        v <- getExpr
+        return $ NamedPatt k p v
+
+nextTag :: StateT MASTContext Get ()
+nextTag = do
+    tag <- lift getWord8
+    case (toEnum . fromEnum) tag of
+        -- Literals
+        'L' -> do
+            literalTag <- lift getWord8
+            expr <- case (toEnum . fromEnum) literalTag of
+                -- 'C' -> do ...
+                'D' -> DoubleExpr <$> getDouble
+                'I' -> do
+                    i <- getVarInt
+                    return $ IntExpr $ zzd i
+                'N' -> return NullExpr
+                'S' -> StrExpr <$> getStr
+            modify (\(exprs, patts) -> (exprs ++ [expr], patts))
+        -- Patts
+        'P' -> do
+            pattTag <- lift getWord8
+            patt <- case (toEnum . fromEnum) pattTag of
+                'A' -> ViaPatt <$> getExpr <*> getPatt
+                'B' -> BindPatt <$> getStr
+                'F' -> FinalPatt <$> getStr <*> getExpr
+                'I' -> IgnorePatt <$> getExpr
+                'L' -> ListPatt <$> getPatts
+                'V' -> VarPatt <$> getStr <*> getExpr
+            modify (\(exprs, patts) -> (exprs, patts ++ [patt]))
+        -- Exprs
+        tag -> do
+            expr <- case tag of
+                'A' -> AssignExpr <$> getStr <*> getExpr
+                'B' -> BindingExpr <$> getStr
+                'C' -> CallExpr <$> getExpr <*> getStr <*> getExprs <*> getNamedExprs
+                'D' -> DefExpr <$> getPatt <*> getExpr <*> getExpr
+                'E' -> EscapeExpr <$> getPatt <*> getExpr <*> getPatt <*> getExpr
+                'F' -> FinallyExpr <$> getExpr <*> getExpr
+                'H' -> HideExpr <$> getExpr
+                'I' -> IfExpr <$> getExpr <*> getExpr <*> getExpr
+                'M' -> MethodExpr <$> getStr <*> getStr <*> getPatts <*> getNamedPatts <*> getExpr <*> getExpr
+                'N' -> NounExpr <$> getStr
+                'O' -> ObjectExpr <$> getStr <*> getPatt <*> getExpr <*> getExprs <*> getExprs <*> getExprs
+                'R' -> MatcherExpr <$> getPatt <*> getExpr
+                'S' -> SequenceExpr <$> getExprs
+                'Y' -> TryExpr <$> getExpr <*> getPatt <*> getExpr
+                'e' -> EscapeOnlyExpr <$> getPatt <*> getExpr
+            modify (\(exprs, patts) -> (exprs ++ [expr], patts))
